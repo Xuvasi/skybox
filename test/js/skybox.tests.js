f533edb0fd2a11b7881b2d1616d0bd153fde14f3
Ben Johnson <benbjohnson@yahoo.com>
2013-02-10 12:40:30 -0700

    Adjust visible nodes calculation.

--- test/js/skybox.tests.js
+++ test/js/skybox.tests.js
@@ -1,4 +1,4 @@
-module("skybox.query.toHTML")
+module("skybox.query.html")
 test("SELECT count() GROUP BY action_id ON :enter", function() {
   var html = skybox.query.html({
     selections:[{
@@ -7,7 +7,7 @@ test("SELECT count() GROUP BY action_id ON :enter", function() {
       conditions: [{type:"on", action:"enter"}]
     }]
   })
-  equal(html, 'Find the <span class="selection">number of actions performed</span> <span class="condition" data-condition-index="0">on session start</span>.')
+  equal(html, 'Find the <span rel="popover" class="selection">number of actions performed</span> <span class="condition" data-condition-index="0">on session start</span>.')
 });
 
 test("SELECT count() GROUP BY action_id ON :enter AFTER <action>", function() {
@@ -24,7 +24,7 @@ test("SELECT count() GROUP BY action_id ON :enter AFTER <action>", function() {
     }]
   })
   equal(html,
-    'Find the <span class="selection">number of actions performed</span> ' +
+    'Find the <span rel="popover" class="selection">number of actions performed</span> ' +
     '<span class="condition" data-condition-index="0">on session start</span> ' +
     'and <span class="condition" data-condition-index="1">after <em>\'/foo\'</em></span>.'
   )
